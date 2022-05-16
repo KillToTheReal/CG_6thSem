a8b64c5d8e1ae206d4ae6b3fbe7c201a156f5273
killtothereal <krakus540@gmail.com>
2022-05-16 20:31:54 +0300

    bugged

--- lab2/2_3.py
+++ lab2/2_3.py
@@ -12,6 +12,7 @@ class TKWindow:
         self.canv = Canvas(self.window, width=800, height=600)
         self.canv.bind("<Button 1>", self.addPoint)
         self.canv.place(x=0,y=0)
+        self.part = True
         self.dots = []
         self.update()
 
@@ -23,16 +24,61 @@ class TKWindow:
 
     def addPoint(self,event):
         self.placeDot(event.x,event.y)
-        self.dots.append(np.array([event.x,event.y]))
+        self.dots.append(np.array([event.x,event.y],dtype=np.float64))
         self.drawBezier()
 
     def showDots(self):
         for i in self.dots:
             self.placeDot(i[0],i[1])
 
-    def drawBezier(self):
-        pass
+    def sw(self):
+        if self.part:
+            self.part = False
+        else:
+            self.part = True    
 
+    def drawBezier(self):
+        if len(self.dots) >=3:
+            points = []
+            delta = 0.01
+            controlPoints = copy.deepcopy(self.dots)
+            if self.part:
+                newContrPoints = []
+                cntrllen = len(controlPoints)
+                for i in range (cntrllen ):
+                    newContrPoints.append(controlPoints[i])
+                    if i < cntrllen - 1:
+                        point_1 = controlPoints[i]
+                        point_2 = controlPoints[i+1]
+                        sum = point_1 + point_2
+                        sum *= 0.5
+                        newContrPoints.append(sum)
+
+                controlPoints = newContrPoints[:]
+            for i in range(len(controlPoints)):
+                 self.placeDot(controlPoints[i][0],controlPoints[i][1])
+
+            degree = 2
+            degreeFac = math.factorial(degree)
+            for j in np.arange(1,len(controlPoints)-degree,degree):
+                for t in np.arange(delta,1,delta):
+                    point = np.array([0,0],dtype=np.float64)
+                    for i in range(degree):
+                        vec = np.array([controlPoints[j+i][0],controlPoints[j+i][1]])
+                        factor = degreeFac / (math.factorial(i) * math.factorial(degree-i))
+                        pw = (1.0 - t)**(degree-i)
+                        pw_t = t**i
+                        appsum = factor * pw * pw_t
+                        vec = vec * appsum
+                        point+=vec 
+                    points.append(point)
+
+            for i in range(len(points)-1):
+                v1 = points[i]
+                self.placeDot(v1[0],v1[1])
+                v2 = points[i+1]
+                self.drawLine(v1[0],v1[1],v2[0],v2[1])
+                
     def clearScreen(self):
         self.canv.delete("all")
 
